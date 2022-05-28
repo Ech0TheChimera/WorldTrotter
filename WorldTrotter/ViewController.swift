f344515f9f6ba3b736e6a6ce47667df388dcabb2
= <=>
2022-05-28 06:33:30 -0700

    Updated to include a gradient background and to space out the text

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -9,5 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Create the gradient
+        let myGradient: CAGradientLayer = CAGradientLayer()
+        
+        // Make the Gradient layer take up the whole frame of our view
+        myGradient.frame = view.bounds
+        
+        // Set the Colors array values
+        myGradient.colors = [UIColor.red.cgColor,
+            UIColor.systemPink.cgColor,
+            UIColor.systemOrange.cgColor]
+        
+        // Find our view -- Find its parent Layer -- Add the gradient to the Layer as a Sub-Layer
+        view.layer.addSublayer(myGradient)
+        
+        // Set our layer's position in the layer heiarchy to 0 -- The back.
+        view.layer.insertSublayer(myGradient, at: 0)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+    }
+    
 }
 
